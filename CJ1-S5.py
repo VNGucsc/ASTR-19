ac57e93c2c156c10bb53f7748b2a1ef07ada5070
Vinh An Nguyen <166468851+VNGucsc@users.noreply.github.com>
2025-10-15 18:49:31 -0700

    Adding Session 5 code.

--- CJ1-S5.py
+++ CJ1-S5.py
@@ -1,8 +1,16 @@
-import numpy as np
-import matplotlib as mp
+import math
 
 def main():
-    print("Something")
+    x = 0.0
+    x_goal = 2 * math.pi
+
+    step_size = x_goal / 999 
+    
+    for i in range(1000):
+        temp = x + i * step_size
+        temp2 = math.sin(temp)
+        print(f'x: {temp} sin(x) = {temp2}')
+
 
 if __name__ == "__main__":
     main()
